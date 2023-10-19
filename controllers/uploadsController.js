9c99302860eb95df754a2744bd4d3f4e76e42a58
Francisco Berti da Cruz <fbc991@live.com>
2023-10-18 21:08:31 -0300

    done

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
 const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
   //test no empty file
   if (!req.files) {
     throw new CustomError.BadRequestError('no file uploaded');
@@ -31,6 +33,15 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    { use_filename: true, folder: 'file-upload' }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = { uploadProductImage };
